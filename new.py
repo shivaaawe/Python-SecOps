4dcb9ba0d90221d9f96c4ee5305a8e3ffe3cb939
Mikkili Shiva Sai Reddy <mikkilishiva@gmail.com>
2025-07-21 23:11:19 +0530

    Learning Python

--- new.py
+++ new.py
@@ -1,2 +1,15 @@
 name = "Shiva"
 print(name)
+
+ip = input("Please enter your IP address ")
+print("The IP address that you hav entered is "+ip)
+
+first_name = "Shiva"
+last_name = "Reddy"
+
+print("Your name is "+first_name+" "+last_name)
+
+fname = input("What is your first name? ")
+lname = input("What is your last name? ")
+
+print(f"Your name is {fname} {lname}")
