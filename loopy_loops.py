535165699c9f1522ba1d306560ae91490abd6a92
Mikkili Shiva Sai Reddy <mikkilishiva@gmail.com>
2025-07-22 12:24:56 +0530

    Loopy Loops

---
+++ loopy_loops.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "pineapple"]
+
+for fruit in fruits:
+    print(fruit+ " pie")
+
+# Rage Range
+
+for num in range(1,10): #Start from 1 and end at 9
+    print(num)
+
+for num in range(0,100,2): #Numbers between 0 and 100, divisible by 2 
+    print(num)
+
+for nu in range(0,100):
+    if nu%2==0:
+        print(nu)
