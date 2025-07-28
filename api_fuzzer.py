5d0df383804470ad1d0371f8fa7e0072e96f3774
Mikkili Shiva Sai Reddy <91215905+shivaaawe@users.noreply.github.com>
2025-07-28 16:36:35 +0530

    API Fuzzer

--- api_fuzzer.py
+++ api_fuzzer.py
@@ -1,18 +1,18 @@
 import requests
-
+import json
 import sys
 
 def come_back_always():
     for word in sys.stdin:
-        res = requests.get(url=f"Please enter the URL of your target machine.{word}")
+        res = requests.get(url=f"Your URL goes here.{word}")
         # URL example - http://10.10.10.10/
         if res.status_code == 404:
             come_back_always()
         else:
             data = res.json()
-            print(data)
-            print(res.status_code)
             print(word)
+            print(res.status_code)
+            print(json.dumps(data,indent=4))
 
 come_back_always()
 
